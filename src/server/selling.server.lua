f1cc3ce356b1b7c72a91c9a82ce40e6c400f4fb2
exx <superjamrs323@gmail.com>
2021-06-16 16:12:27 -0700

    Added some error handling

--- src/server/selling.server.lua
+++ src/server/selling.server.lua
@@ -1,10 +1,15 @@
 -- THIS SCRIPT WAS CREATED WITH ROJO; ANY CHANGES MADE IN STUDIO WILL BE OVERWRITTEN
 
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 Players.PlayerAdded:Connect(function(player)
     player.CharacterAdded:Connect(function(character)
-        local gui = game.ReplicatedStorage.ESell:Clone()
-        gui.Parent = player.Character:WaitForChild("HumanoidRootPart")
+        if ReplicatedStorage.ESell then
+            local gui = game.ReplicatedStorage.ESell:Clone()
+            gui.Parent = player.Character:WaitForChild("HumanoidRootPart")
+        else
+            warn("ESell not found in ReplicatedStorage- selling may not function properly.")
+        end
     end)
 end)
