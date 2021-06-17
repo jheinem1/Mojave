381ba4bd9bc24762666388f1df1c92b311a81992
exx <superjamrs323@gmail.com>
2021-06-16 17:11:02 -0700

    removed redundant files for now

--- src/client/nametags/nametag.lua
+++
@@ -1,7 +0,0 @@
--- THIS SCRIPT WAS CREATED WITH ROJO; ANY CHANGES MADE IN STUDIO WILL BE OVERWRITTEN
-
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Roact = require(ReplicatedStorage.Vendor.Roact)
-
-local Nametag = Roact.Component:extend("Nametag")
-
--- src/client/nametags/tooltip.lua
+++
@@ -1,8 +0,0 @@
--- THIS SCRIPT WAS CREATED WITH ROJO; ANY CHANGES MADE IN STUDIO WILL BE OVERWRITTEN
-
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Roact = require(ReplicatedStorage.Vendor.Roact)
-
-local PlayerToolTip = Roact.Component:extend("PlayerToolTip")
-
-local message = Instance.new("Message")
