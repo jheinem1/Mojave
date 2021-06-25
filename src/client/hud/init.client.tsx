a0a9f5bd8933bc59560c4511597600daf4fef1b9
Laptop <superjamrs323@gmail.com>
2021-06-25 11:38:39 -0700

    Re-enabled health

--- src/client/hud/init.client.tsx
+++ src/client/hud/init.client.tsx
@@ -2,5 +2,5 @@ import Roact from "@rbxts/roact";
 import { Players, StarterGui } from "@rbxts/services";
 import { HudComponent } from "./hud_component";
 
-StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.Health, false);
+// StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.Health, false);
 Roact.mount(<HudComponent />, Players.LocalPlayer.FindFirstChildOfClass("PlayerGui"));
