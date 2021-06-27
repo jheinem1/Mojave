4367c0b81ecbc8aea59580e4adf2f25e5cb98777
exx <superjamrs323@gmail.com>
2021-06-26 20:02:23 -0700

    fixed compilation issue

---
+++ out/client/hud/init.client.lua
@@ -0,0 +1,7 @@
+-- Compiled with roblox-ts v1.1.1
+local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
+local Roact = TS.import(script, TS.getModule(script, "roact").src)
+local Players = TS.import(script, TS.getModule(script, "services")).Players
+local HudComponent = TS.import(script, script, "hud_component").HudComponent
+-- StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.Health, false);
+Roact.mount(Roact.createElement(HudComponent), Players.LocalPlayer:FindFirstChildOfClass("PlayerGui"))
