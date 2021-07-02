ed35010e5166fb36ba8d9c30ef8420741f46182a
exx <superjamrs323@gmail.com>
2021-07-02 01:53:49 -0700

    Dumbed down regions to only accept parts as arguments

--- src/shared/region.ts
+++ src/shared/region.ts
@@ -1,46 +1,19 @@
 import { RunService } from "@rbxts/services";
-import { t } from "@rbxts/t";
-
-const isCharacter = t.union(t.instanceIsA("Model"), t.children({
-    HumanoidRootPart: t.instanceIsA("Part"),
-    Humanoid: t.instanceIsA("Humanoid"),
-    Head: t.instanceOf("BasePart")
-}));
-
-type Character = Model & {
-    HumanoidRootPart: Part,
-    Humanoid: Humanoid,
-    Head: BasePart
-}
 
 abstract class Region {
     /**
-     * Gets the HumanoidRootPart of a player or character, throws an error if invalid character
-     */
-    protected getRoot(object: Player | Model): Part {
-        if (t.instanceIsA("Player")(object))
-            return object.Character ? this.getRoot(object.Character) : this.getRoot(object.CharacterAdded.Wait()[0]);
-        else if (isCharacter(object))
-            return (object as Character).HumanoidRootPart;
-        else
-            throw "Invalid character model!"
-    }
-    /**
-     * @param object A valid player or character, will throw error if missing properties
      * @returns A promise that resolves when a player enters the region
      */
-    abstract enteredRegion(object: Player | Model): Promise<void>;
+    abstract enteredRegion(part: BasePart): Promise<void>;
     /**
-     * @param object A valid player or character, will throw error if missing properties
      * @returns A promise that resolves when a player leaves the region
      */
-    abstract leftRegion(object: Player | Model): Promise<void>;
+    abstract leftRegion(part: BasePart): Promise<void>;
     /**
      * Checks if the player/character is inside the region.
-     * @param object A valid player or character, will throw error if missing properties.
      * @returns True if the player is in the region, false if not
      */
-    abstract isInRegion(object: Player | Model): boolean;
+    abstract isInRegion(part: BasePart): boolean;
 }
 
 
@@ -59,36 +32,34 @@ export class BasePartRegion extends Region {
         newPart.CanCollide = false;
         this.part = newPart;
     }
-    async enteredRegion(object: Model | Player): Promise<void> {
-        const root = this.getRoot(object);
-        if (root.GetTouchingParts().some(part => part === this.part))
+    async enteredRegion(part: BasePart): Promise<void> {
+        if (part.GetTouchingParts().some(part => part === this.part))
             return;
         return new Promise(resolve => {
             let connection: RBXScriptConnection
             connection = this.part.Touched.Connect(hit => {
-                if (hit === root) {
+                if (hit === part) {
                     connection.Disconnect();
                     resolve();
                 }
             });
         });
     }
-    async leftRegion(object: Model | Player): Promise<void> {
-        const root = this.getRoot(object);
-        if (root.GetTouchingParts().some(part => part === this.part))
+    async leftRegion(part: BasePart): Promise<void> {
+        if (part.GetTouchingParts().some(part => part === this.part))
             return;
         return new Promise(resolve => {
             let connection: RBXScriptConnection
             connection = this.part.TouchEnded.Connect(hit => {
-                if (hit === root) {
+                if (hit === part) {
                     connection.Disconnect();
                     resolve();
                 }
             });
         });
     }
-    isInRegion(object: Model | Player): boolean {
-        return this.getRoot(object).GetTouchingParts().some(part => part === this.part);
+    isInRegion(part: BasePart): boolean {
+        return part.GetTouchingParts().some(part => part === this.part);
     }
 }
 
@@ -103,39 +74,36 @@ export class SphereRegion extends Region {
         this.center = sphere.Position;
         this.radius = math.min(sphere.Size.X, sphere.Size.Y, sphere.Size.Z);
     }
-    async enteredRegion(object: Model | Player): Promise<void> {
-        const root = this.getRoot(object);
+    async enteredRegion(part: BasePart): Promise<void> {
         return new Promise(resolve => {
             let connection: RBXScriptConnection
             connection = RunService.Heartbeat.Connect(() => {
-                if ((root.Position.sub(this.center)).Magnitude <= this.radius) {
+                if ((part.Position.sub(this.center)).Magnitude <= this.radius) {
                     connection.Disconnect();
                     resolve();
                 }
             });
         });
     }
-    async leftRegion(object: Model | Player): Promise<void> {
-        const root = this.getRoot(object);
+    async leftRegion(part: BasePart): Promise<void> {
         return new Promise(resolve => {
             let connection: RBXScriptConnection
             connection = RunService.Heartbeat.Connect(() => {
-                if ((root.Position.sub(this.center)).Magnitude > this.radius) {
+                if ((part.Position.sub(this.center)).Magnitude > this.radius) {
                     connection.Disconnect();
                     resolve();
                 }
             });
         });
     }
-    isInRegion(object: Model | Player): boolean {
-        return (this.getRoot(object).Position.sub(this.center)).Magnitude <= this.radius;
+    isInRegion(part: BasePart): boolean {
+        return (part.Position.sub(this.center)).Magnitude <= this.radius;
     }
     /**
-     * @param object A valid player or character, will throw error if missing properties.
      * @returns The character's distance from the center
      */
-    getDistance(object: Model | Player): number {
-        return (this.getRoot(object).Position.sub(this.center)).Magnitude
+    getDistance(part: BasePart): number {
+        return (part.Position.sub(this.center)).Magnitude
     }
 }
 
@@ -145,33 +113,29 @@ export class SphereRegion extends Region {
 export class RegionUnion {
     constructor(public regions: Region[]) { }
     /**
-     * @param object A valid player or character, will throw error if missing properties
      * @returns A promise that resolves when a player enters a region
      */
-    async enteredRegion(object: Player | Model) {
-        return Promise.race(this.regions.map(region => region.enteredRegion(object)));
+    async enteredRegion(part: BasePart) {
+        return Promise.race(this.regions.map(region => region.enteredRegion(part)));
     }
     /**
-     * @param object A valid player or character, will throw error if missing properties
      * @returns A promise that resolves when a player leaves a region
      */
-    async leftRegion(object: Player | Model) {
-        return Promise.race(this.regions.map(region => region.leftRegion(object)));
+    async leftRegion(part: BasePart) {
+        return Promise.race(this.regions.map(region => region.leftRegion(part)));
     }
     /**
      * Checks if the player/character is inside all regions.
-     * @param object A valid player or character, will throw error if missing properties
      * @returns An array of regions (if any) the player is in
      */
-    isInRegions(object: Player | Model): Region[] {
-        return this.regions.filter(region => region.isInRegion(object));
+    isInRegions(part: BasePart): Region[] {
+        return this.regions.filter(region => region.isInRegion(part));
     }
     /**
      * Checks if the player/character is inside a single region.
-     * @param object A valid player or character, will throw error if missing properties
      * @returns A region (if any) the player is in
      */
-    isInRegion(object: Player | Model): Region | undefined {
-        return this.regions.find(region => region.isInRegion(object));
+    isInRegion(part: BasePart): Region | undefined {
+        return this.regions.find(region => region.isInRegion(part));
     }
 }
