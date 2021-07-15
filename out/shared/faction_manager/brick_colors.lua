b2274576147abf7c8c356ed363872728001f13d1
exx <superjamrs323@gmail.com>
2021-07-15 15:11:39 -0700

    Compiled previous commits

---
+++ out/shared/faction_manager/brick_colors.lua
@@ -0,0 +1,20 @@
+local usedColors = {}
+
+function getColor(choice: string | nil): BrickColor
+    local color = choice and tonumber(choice) and BrickColor.new(tonumber(choice)) or BrickColor.new(choice)
+    if color and not usedColors[color.Number] then
+        return color
+    else
+        for i = 1, 1032 do
+            if not usedColors[i] then
+                color = BrickColor.new(i)
+                if not usedColors[color.Number] then
+                    return color
+                end
+            end
+        end
+        error("No available colors!")
+    end
+end
+
+return { getColor = getColor }
