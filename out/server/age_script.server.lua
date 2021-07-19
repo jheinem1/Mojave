a67149a099dd6c23aba4d396ac1c23dc298102ee
exx <superjamrs323@gmail.com>
2021-07-18 23:45:43 -0700

    Updated age script to not run in studio

--- out/server/age_script.server.lua
+++ out/server/age_script.server.lua
@@ -1,8 +1,12 @@
 -- Compiled with roblox-ts v1.2.2
 local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
-local Players = TS.import(script, TS.getModule(script, "@rbxts", "services")).Players
-Players.PlayerAdded:Connect(function(player)
-	if player.AccountAge < 7 then
-		player:Kick("Your account must be at least a week old in order to play!")
-	end
-end)
+local _services = TS.import(script, TS.getModule(script, "@rbxts", "services"))
+local Players = _services.Players
+local RunService = _services.RunService
+if not RunService:IsStudio() then
+	Players.PlayerAdded:Connect(function(player)
+		if player.AccountAge < 7 then
+			player:Kick("Your account must be at least a week old in order to play!")
+		end
+	end)
+end
