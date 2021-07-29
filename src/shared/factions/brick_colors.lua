2b464e63ca693e832d88a93c4ee94a601136a2a7
exx <superjamrs323@gmail.com>
2021-07-29 14:02:41 -0700

    (permanantly) fixed team color assignment issues

--- src/shared/factions/brick_colors.lua
+++ src/shared/factions/brick_colors.lua
@@ -3,15 +3,14 @@ local usedColors = {[BrickColor.Gray().Number] = true}
 function getColor(choice: string | nil): BrickColor
     local color = choice and tonumber(choice) and BrickColor.new(tonumber(choice)) or BrickColor.new(choice)
     if color and not usedColors[color.Number] then
+        usedColors[color.Number] = true
         return color
     else
         for i = 1, 1032 do
-            if not usedColors[i] then
-                color = BrickColor.new(i)
-                if not usedColors[color.Number] then
-                    usedColors[color.Number] = true
-                    return color
-                end
+            color = BrickColor.new(i)
+            if not usedColors[color.Number] then
+                usedColors[color.Number] = true
+                return color
             end
         end
         error("No available colors!")
